e0e341e3e0d0525f634445807dc0f4f6c3b88b68
Octave <octave.blandet@etu.unistra.fr>
2023-04-26 17:44:24 +0200

    convert to object

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # Projet_Pacman_Cpp
+
+cmake ./ && make && ./PacMan/usr/include/SDL2
+/usr/include/SDL2
---
+++ src/main.cpp
@@ -0,0 +1,9 @@
+#include "pacman.h"
+
+int main(int argc, char **argv)
+{
+    Pacman pacman{};
+    pacman.start();
+
+    return 0;
+}
